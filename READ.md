2a0daf3db78ee329e5acec601585a1aa8135cbdb
nayrlaw <nayrlaw0904@gmail.com>
2026-05-31 12:50:21 +0800

    Add project README and update configurations
    - Add main project README.md
    - Update Express backend with CORS headers
    - Update weather app README with screenshot

---
+++ READ.md
@@ -0,0 +1,7 @@
+# Hong Kong Weather App
+
+A React weather application that displays current weather information for Hong Kong, fetching data from the Hong Kong Observatory API.
+
+## Screenshot
+
+![Weather App Screenshot](./image.png)
--- express-bn/main.ts
+++ express-bn/main.ts
@@ -6,6 +6,18 @@ const port = process.env.PORT || 9453;
 
 const app = express();
 
+// Add CORS headers manually
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // Apply root routes
 app.use("/", createRoutes());
 
